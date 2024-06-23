c752af2551caf4b962a113a236c945ec97af6aa8
devanshwebhost <devanshrajput032006@gmail.com>
2024-06-23 16:54:35 +0530

    server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const app = express();
-const port = process.env.PORT || 3000; // Use the provided PORT environment variable or default to 3000
+const port = process.env.PORT || 3000;
 
 // Middleware to parse request bodies
 app.use(bodyParser.urlencoded({ extended: true }));
